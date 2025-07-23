cb345363c3cda1ced83521a2ac47da2ad343740f
8bitprodigy <chrisxdeboy@gmail.com>
2025-07-23 02:16:56 -0700

    Forgot to move the `Head` struct definition into the `_head_.h` private header.

--- src/_head_.h
+++ src/_head_.h
@@ -2,3 +2,22 @@
 
 
 
+typedef struct 
+Head
+{
+    Camera3D              camera;
+    RenderTexture         viewport;
+    int                   controller_id;
+    Engine               *engine;
+    Entity               *controlled_entity;
+    void                 *user_data;
+    FreeUserDataCallback *FreeUserData;
+
+    struct Head
+		*prev,
+		*next;
+
+	HeadVTable *vtable;
+} 
+Head;
+
